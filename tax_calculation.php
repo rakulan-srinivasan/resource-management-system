1ba94d054fdfaeb8411e3cc09f37fbaa52ace751
rakulan-srinivasan <vsriabilarag@live.com>
2022-11-22 23:40:51 +0530

    Added tax_calculation

---
+++ tax_calculation.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Income tax calculation</title>
+</head>
+<body>
+    <style>
+    .data, .data_head{
+        display:flex;
+        padding: 5px;
+    }
+
+
+    </style>
+
+
+
+
+
+<div class="data_head">
+
+<form class = "data" method="post">
+    <p>
+    NAME: <input class="t" type="text"></input><br>
+    Gender<input type="radio" name="gender" value="f">Female</input><input type="radio" name="gender" value="m">Male</input><br>
+    Pan Number: <input type="text" name="pan"></input>
+    Gross Pay: <input type="text" name="pan"></input>
+    Housing loan interest: <input type="text" name="pan"></input>
+    Pan Number: <input type="text" name="pan"></input>
+    Pan Number: <input type="text" name="pan"></input>
+    Pan Number: <input type="text" name="pan"></input>
+    Pan Number: <input type="text" name="pan"></input>
+
+
+    
+
+
+</form>
+</div>   
+</body>
+</html>
